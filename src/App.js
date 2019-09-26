5b8c6e8805cd9132d348a4df7848ab5cbb7593dd
Felipe Trova <felipe.trova@starcorp.tech>
2019-09-26 15:01:05 -0500

    Create a new base layout and navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main'
+import Home from './components/Home'
 import 'bulma'
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/" component={Main} />
+        <Route path="/home" component={Home} />
+      </div>
+    </Router>
   );
 }
 
